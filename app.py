55ec0869089c2fb02daab92239611501660d3f73
Chernenko <pichernenko@gmail.com>
2023-11-06 11:37:27 +0700

    update

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ import streamlit as st
 from backend import *
 
 
+initialize_totalan(state_names=['total_aset_kas']) #set the st.session_state.total_aset_kas = 0
 
-def number_input (labelnya,keynya,helpnya=''):
-    st.number_input(label=labelnya
-        ,value=default_min_value,min_value=default_min_value
-        ,on_change=callback_input
-        ,key=keynya
-        ,help=helpnya
-    )
-tab1,tab2 = st.tabs(['Aset','Kewajiban'])
+def number_input (labelnya,keynya,helpnya='',default_min_value=0):
+    
+    st.session_state.keynya = st.number_input(label=labelnya
+                             ,value=default_min_value
+                             ,min_value=default_min_value
+                             ,on_change=callback_input
+                             ,key=keynya
+                             ,help=helpnya)
+    
+
+    
+tab1,tab2,tab3,tab4,tab5 = st.tabs(['Aset','Kewajiban','Pendapatan','Pengeluaran','Debug'])
 default_min_value = 0
 
 
@@ -27,7 +32,8 @@ with tab1:
             number_input('Kas di Tangan','aset_lancar__kas_di_tangan')
             number_input('Tabungan','aset_lancar__tabungan')
             number_input('Deposito','aset_lancar__deposito')
-            number_input('Reksadana Pasar Uang','aset_lancar__rdpu')            
+            number_input('Reksadana Pasar Uang','aset_lancar__rdpu')
+            # st.subheader("Total Aset Kas: {}".format(st.session_state.total_aset_kas)) #uncomment this to see the error
         with col2:        
             st.subheader("Aset Investasi")
             number_input('Emas Logam Mulia','aset_lancar__emas')                       
@@ -55,4 +61,49 @@ with tab1:
             number_input("Properti","aset_tidak_lancar__properti")
             number_input("Tanah","aset_tidak_lancar__tanah")
             number_input("Nilai Bersih Usaha","aset_tidak_lancar__bisnis")
-            print("Hello world")
+with tab2:
+    col1,col2 = st.columns(2)
+    with col1:
+        st.subheader("Jangka Pendek")
+        number_input("Kartu Kredit",'kewajiban_jangka_pendek__kartu_kredit')
+        number_input("Pinjaman Pribadi",'kewajiban_jangka_pendek__pinjaman_pribadi')    
+        number_input("Pinjaman Mobil",'kewajiban_jangka_pendek__pinjaman_mobil')
+        number_input("Pinjaman KTA (Kredit Tanpa Agunan)",'kewajiban_jangka_pendek__kta')                          
+        number_input("Lainnya",'kewajiban_jangka_pendek__lainnya')        
+    with col2:
+        st.subheader("Jangka Panjang")
+        number_input("Pinjaman Rumah",'kewajiban_jangka_panjang__pinjaman_rumah')
+        number_input("Pinjaman Apartemen",'kewajiban_jangka_panjang__pinjaman_apartement')        
+        number_input("Pinjaman dari Perusahaan",'kewajiban_jangka_panjang__pinjaman_perusahaan')        
+        number_input("Pinjaman lainnya",'kewajiban_jangka_panjang__kartu_kredit')        
+
+with tab3:
+    st.subheader("Pendapatan rutin per bulan")
+    number_input("Gaji",'pendapatan_rutin__gaji')
+    number_input("Bagi hasil bisnis",'pendapatan_rutin__bagi_hasil_bisnis')        
+    number_input("Pendapatan Sewa",'pendapatan_rutin__pendapatan_sewa')
+    number_input("Pendapatan Lain-lain",'pendapatan_rutin__lain_lain')     
+
+with tab4:
+    st.subheader("Pengeluaran rutin per bulan")
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        number_input("Gaji",'pengeluaran_rutin__tabungan_investasi')
+        number_input("Zakat/Perpuluhan/Sedekah",'pengeluaran_rutin__zakat_perpuluhan_sedekah')
+        number_input("Premi Asuransi",'pengeluaran_rutin__asuransi')    
+        number_input("Listrik",'pengeluaran_rutin__tabungan_listrik')    
+        number_input("Air / PDAM",'pengeluaran_rutin__tabungan_air')
+    with col2:
+        number_input("Pulsa / Paket Data Smartphone",'pengeluaran_rutin__telepon')
+        number_input("Internet",'pengeluaran_rutin__internet')
+        number_input("Belanja bulanan / dapur",'pengeluaran_rutin__belanja_dapur')
+        number_input("Transportasi",'pengeluaran_rutin__transportasi')    
+        number_input("Kesehatan",'pengeluaran_rutin__kesehatan')    
+    with col3:
+        number_input("Pendidikan",'pengeluaran_rutin__pendidikan')    
+        number_input("Belanja Pribadi",'pengeluaran_rutin__belanja_pribadi')    
+        number_input("Pengeluaran Anak",'pengeluaran_rutin__pengeluaran_anak')
+        number_input("Cicilan utang",'pengeluaran_rutin__cicilan_utang')
+        number_input("Gaya Hidup",'pengeluaran_rutin__gaya')      
+with tab5:
+    st.write(st.session_state)
--- backend.py
+++ backend.py
@@ -1,39 +1,14 @@
 import streamlit as st
 import re
 
+def initialize_totalan(state_names,default_value=0):
+    for state_name in state_names:
+        if state_name not in st.session_state:
+            st.session_state.state_name = default_value
+
 def callback_input():
-    if 'data' not in st.session_state:
-        st.session_state['data'] = {}
-    
-    st.session_state.data = {
-        'details':{
-            'aset':{
-                'aset_lancar':{
-                    'aset_kas':{
-                        'kas_di_tangan':st.session_state.aset_lancar__kas_di_tangan,
-                        'tabungan': st.session_state.aset_lancar__tabungan,
-                        'deposito': st.session_state.aset_lancar__deposito,
-                        'rdpu':st.session_state.aset_lancar__rdpu
-                    },
-                    'aset_investasi':{
-                        'emas':st.session_state.aset_lancar__emas,
-                        'rdpt':st.session_state.aset_lancar__rdpt,
-                        'rdc':st.session_state.aset_lancar__rdc,
-                        'rds':st.session_state.aset_lancar__rds,
-                        'obligasi':st.session_state.aset_lancar__obligasi,
-                        'saham':st.session_state.aset_lancar__saham,
-                        'polis':st.session_state.aset_lancar__polis,
-                        'others':st.session_state.aset_lancar__others
-
-                    }
-                },
-                'aset_tidak_lancar':{
-
-                }
-            },
-            'kewajiban':{
-
-            }
-        }
-    }
-    st.write(st.session_state.data)
+    st.session_state['total_aset_kas']= st.session_state.aset_lancar__kas_di_tangan 
+    + st.session_state.aset_lancar__tabungan
+    + st.session_state.aset_lancar__rdpu
+    + st.session_state.aset_lancar__deposito
+        
